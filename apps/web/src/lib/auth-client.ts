2dfe6b49263b9cc4e44311a13b5633c7077009ab
Pálmi Þór Valgeirsson <palmithor@gmail.com>
2026-04-16 10:38:37 +0000

    feat!: remove better-auth API key functionality and device API (#637)

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { passkeyClient } from "@better-auth/passkey/client";
-import { apiKeyClient, organizationClient, adminClient } from "better-auth/client/plugins";
+import { organizationClient, adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-	plugins: [organizationClient({}), passkeyClient(), apiKeyClient(), adminClient()],
+	plugins: [organizationClient({}), passkeyClient(), adminClient()],
 });
